9bf60fe0efea97c0fc7e4b2405fcd3e52dbd1541
Donald Ubuntu <donaldptngai@gmail.com>
2023-08-15 17:19:12 -0400

    Fixed while is not none and elif to check vals

--- mergeSortedLists.py
+++ mergeSortedLists.py
@@ -1,24 +1,27 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        print ("hi")
         if list1 is None:
+            print("returning none 1")
             return list2
         if list2 is None:
+            print ("returning none 2")
             return list1
         
         firstItemSecondList = list2
         firstItemFirstList = list1
         zeroItemFirstList = list1
-        
-        while not list2:
+        while firstItemSecondList is not None:
             #take first list
             #take first item from second list
             if firstItemFirstList is None:
+                print("returning none 3")
                 return list2
 
             #Determine whether the first item from the second list is greater than, equal to or less than 1.
@@ -27,16 +30,19 @@ class Solution:
                 # update items and redo test
                 zeroItemFirstList = firstItemFirstList
                 firstItemFirstList = firstItemFirstList.next
+                print ("Moving onto next")
 
             #If equal to 2 then put the number on the right
-            elif firstItemSecondList == firstItemFirstList: 
+            elif firstItemSecondList.val == firstItemFirstList.val: 
                 firstItemSecondList.next = firstItemFirstList.next
                 firstItemFirstList.next = firstItemSecondList
+                print ("Inserting to the right")
 
             #If less than 2 then put the number on the left
-            elif firstItemSecondList < firstItemFirstList: 
+            elif firstItemSecondList.val < firstItemFirstList.val: 
                 zeroItemFirstList.next = firstItemSecondList
                 firstItemSecondList.next = firstItemFirstList
+                print ("Inserting to the left")
             #remove from second list
             firstItemSecondList = firstItemSecondList.next
         return list1
