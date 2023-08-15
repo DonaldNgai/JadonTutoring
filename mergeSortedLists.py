6dfb6ede47952402a95d759fd66f17b71d3c7c28
Donald Ubuntu <donaldptngai@gmail.com>
2023-08-15 17:57:06 -0400

    First attempt at simplified

--- mergeSortedLists.py
+++ mergeSortedLists.py
@@ -14,52 +14,42 @@ class Solution:
             print ("returning none 2")
             return list1
         
-        firstItemSecondList = list2
-        firstItemFirstList = list1
-        zeroItemFirstList = list1
-        while firstItemSecondList is not None:
-            print("A0:{} A1:{} B1:{}".format(zeroItemFirstList.val, firstItemFirstList.val, firstItemSecondList.val))
+        # Guarunteed that either list 1 or list 2 is not null here
+        sortedListHead = ListNode()
+        currentListNode = sortedListHead
+        currentL1Node = list1
+        currentL2Node = list2
+        while currentL1Node is not None and currentL2Node is not None:
+            printAll(sortedListHead)
+            if currentL1Node is None:
+                currentListNode.val = currentL2Node.val
+                currentListNode.next = currentL2Node.next
+                currentL2Node = currentL2Node.next
+                currentListNode = currentListNode.next
+                print("L1 is empty")
 
-            #take first list
-            #take first item from second list
-            if firstItemFirstList is None:
-                print("returning none 3")
-                return list2
-
-            #Determine whether the first item from the second list is greater than, equal to or less than 1.
-            #If greater than 2 then check if its greater than the next number
-            if firstItemSecondList.val > firstItemFirstList.val:
-                # update items and redo test
-                zeroItemFirstList = firstItemFirstList
-                firstItemFirstList = firstItemFirstList.next
-                print ("Moving onto next")
-
-            #If equal to 2 then put the number on the right
-            elif firstItemSecondList.val == firstItemFirstList.val: 
-                firstItemFirstList.next = ListNode(firstItemSecondList.val, firstItemFirstList.next)
-
-                #Look at the next one
-                firstItemSecondList = firstItemSecondList.next
-                firstItemFirstList = list1
-                zeroItemFirstList = list1
-                
-                print ("Inserting to the right")
-                printAll(list1)
-
-            #If less than 2 then put the number on the left
-            elif firstItemSecondList.val < firstItemFirstList.val: 
-                zeroItemFirstList.next = ListNode(firstItemSecondList.val, firstItemFirstList)
-                
-                #Look at the next one
-                firstItemSecondList = firstItemSecondList.next
-                firstItemFirstList = list1
-                zeroItemFirstList = list1
-                
-                print ("Inserting to the left")
-                printAll(list1)
-            #remove from second list
+            elif currentL2Node is None:
+                currentListNode.val = currentL1Node.val
+                currentListNode.next = currentL1Node.next
+                currentL1Node = currentL1Node.next
+                currentListNode = currentListNode.next
+                print("L2 is empty")
             
-        return list1
+            elif currentL1Node.val >= currentL2Node.val:
+                currentListNode.val = currentL2Node.val
+                currentListNode.next = currentL2Node.next
+                currentL2Node = currentL2Node.next
+                currentListNode = currentListNode.next
+                print("L1 >= L2 ")
+
+            elif currentL1Node.val < currentL2Node.val:
+                currentListNode.val = currentL1Node.val
+                currentListNode.next = currentL1Node.next
+                currentL1Node = currentL1Node.next
+                currentListNode = currentListNode.next
+                print("L1 < L2 ")
+            
+        return sortedListHead
 
 def printAll(list1: Optional[ListNode]):
     currentNode = list1
