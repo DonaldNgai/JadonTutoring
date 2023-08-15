54ebc7ae672048bb6a00f411777dbd3916a7a3ea
Donald Ubuntu <donaldptngai@gmail.com>
2023-08-15 17:10:40 -0400

    Next Update

--- mergeSortedLists.py
+++ mergeSortedLists.py
@@ -3,37 +3,46 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-       while list2.empty() != true:
-            
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        
+        firstItemSecondList = list2
+        firstItemFirstList = list1
+        zeroItemFirstList = list1
+        
+        while not list2:
             #take first list
             #take first item from second list
-            firstItemSecondList = list2[0]
-            
-            firstListIndex = 0;
-            firstItemFirstList = list1[firstListIndex]
-            
-            while(firstItemSecondList > firstItemFirstList):
-                firstListIndex += 1
-                if firstListIndex == len(list1):
-                    break
-                firstItemFirstList = list1[firstListIndex]
+            if firstItemFirstList is None:
+                return list2
 
             #Determine whether the first item from the second list is greater than, equal to or less than 1.
             #If greater than 2 then check if its greater than the next number
-            if firstItemSecondList > firstItemFirstList:
-                list1.insert(1, firstItemSecondList)
+            if firstItemSecondList.val > firstItemFirstList.val:
+                # update items and redo test
+                zeroItemFirstList = firstItemFirstList
+                firstItemFirstList = firstItemFirstList.next
 
             #If equal to 2 then put the number on the right
-            if firstItemSecondList == firstItemFirstList: 
-                list1.insert(1, firstItemSecondList)
+            elif firstItemSecondList == firstItemFirstList: 
+                firstItemSecondList.next = firstItemFirstList.next
+                firstItemFirstList.next = firstItemSecondList
+
             #If less than 2 then put the number on the left
-            if firstItemSecondList < firstItemFirstList: 
-                list1.insert(0, firstItemSecondList)
-
+            elif firstItemSecondList < firstItemFirstList: 
+                zeroItemFirstList.next = firstItemSecondList
+                firstItemSecondList.next = firstItemFirstList
             #remove from second list
-            list2.remove(0)
+            firstItemSecondList = firstItemSecondList.next
+        return list1
 
-    def checkPosition(firstItemSecondList,firstItemFirstList,secondItemFirstList):
-        
+
+        # Change to look back
+        # Changed to use class
+        # Changed to elif
+        # Added null cases
