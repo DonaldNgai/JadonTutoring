dd04189ef9272369c76c705b2cb745a30de4a0da
Donald Ngai <donaldptngai@gmail.com>
2023-08-17 15:53:15 -0400

    Added all logic but got caught in a circular reference

--- mergeSortedLists.py
+++ mergeSortedLists.py
@@ -1,30 +1,58 @@
+from typing import Optional
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-       while list2.empty() != true:
+    def printAll(list1: ListNode):
+        currentNode = list1
+        while currentNode is not None:
+            print(currentNode.val)
+            currentNode = currentNode.next
+
+    def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        firstItemSecondList = list2
+        firstItemFirstList = list1
+        zeroItemFirstList = list1
+        while firstItemSecondList is not None:
             #take first list
             #take first item from second list
-            firstItemSecondList = list2[0]
-            firstItemFirstList = list1[0]
-            secondItemFirstList = list1[1]
+            print("A0:{} A1:{} B1:{}".format(zeroItemFirstList.val, firstItemFirstList.val, firstItemSecondList.val))
+
+            # If we've reached the end of first list, add item to the right
+            if firstItemFirstList is None:
+                print ("I'm in insert to right")
+                zeroItemFirstList.next = firstItemSecondList
+                Solution.printAll(list1)
+
             #Determine whether the first item from the second list is greater than, equal to or less than 1.
             #If greater than 2 then check if its greater than the next number
-            if firstItemSecondList > firstItemFirstList:
-                if firstItemSecondList > secondItemFirstList:
+            if firstItemSecondList.val > firstItemFirstList.val:
+                print ("I'm changing positions")
+                zeroItemFirstList = firstItemFirstList
+                firstItemFirstList = firstItemFirstList.next
 
             #If equal to 2 then put the number on the right
-            if firstItemSecondList = firstItemFirstList: 
-                list1.insert(1, firstItemSecondList)
+            elif firstItemSecondList.val == firstItemFirstList.val: 
+                print ("I'm in insert to left because equal")
+                firstItemSecondList.next = firstItemFirstList
+                zeroItemFirstList.next = firstItemSecondList
+                firstItemSecondList = firstItemSecondList.next
+                Solution.printAll(list1)
+                
             #If less than 2 then put the number on the left
-            if firstItemSecondList < firstItemFirstList: 
-                list1.insert(0, firstItemSecondList)
+            elif firstItemSecondList.val < firstItemFirstList.val: 
+                print ("I'm in insert to left because less than")
+                firstItemSecondList.next = firstItemFirstList
+                zeroItemFirstList.next = firstItemSecondList
+                firstItemSecondList = firstItemSecondList.next
+                Solution.printAll(list1)
 
-            #remove from second list
-            list2.remove(0)
 
-    def checkPosition(firstItemSecondList,firstItemFirstList,secondItemFirstList):
-        
+list1 = ListNode(1,ListNode(3, ListNode(5,None)))
+list2 = ListNode(1,ListNode(2, ListNode(4,None)))
+
+Solution.mergeTwoLists(list1 = list1, list2 = list2)
