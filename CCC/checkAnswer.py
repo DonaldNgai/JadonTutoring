6bedddf02f34926d92bf16c21a7081cf50cc6d7d
Donald Ngai <donaldptngai@gmail.com>
2023-09-07 17:08:53 -0400

    Add check answer function

--- CCC/checkAnswer.py
+++ CCC/checkAnswer.py
@@ -7,25 +7,29 @@ def getLines(filepath):
             testsite_array.append(int(line))
     return testsite_array
 
-def getInputs():
-    rel_path = "junior\j1\j1.01.in"
+def getInputs(path):
+    
     file_dir = os.path.dirname(os.path.realpath('__file__'))
-    abs_file_path = os.path.join(file_dir, rel_path)
+    abs_file_path = os.path.join(file_dir, path)
     print (abs_file_path)
     input = getLines(abs_file_path)
-    for key, value in enumerate(input):
-        print("value", value)
-        print("key", key)
+    # for key, value in enumerate(input):
+    #     print("value", value)
+    #     print("key", key)
     return input
 
-def checkOutput():
-    rel_path = "junior\j1\j1.01.out"
-    abs_file_path = os.path.join(file_dir, rel_path)
+def checkAnswer(path, answer):
+    file_dir = os.path.dirname(os.path.realpath('__file__'))
+    abs_file_path = os.path.join(file_dir, path)
     output = getLines(abs_file_path)
-    for key, value in enumerate(output):
-        print("value", value)
-        print("key", key)
-
+    # for key, value in enumerate(output):
+    #     print("value", value)
+    #     print("key", key)
+    if answer == output[0]:
+        print("WOOOHOOOOOO")
+    else:
+        print ("Incorrect")
+     
 
     # print(output[key])
 
--- CCC/j1.py
+++ CCC/j1.py
@@ -2,4 +2,6 @@
 # functions defined in test.py
 from checkAnswer import *
 
-j1Input = getInputs()
+rel_path = "junior\j1\j1.01.in"
+j1Input = getInputs(rel_path)
+checkAnswer(rel_path,10)
